7d65f67a3b010b7031e139ecb7e3d909f20222b8
mrASS0 <108096319+mrASS0@users.noreply.github.com>
2025-07-08 18:20:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-meme_dict = {
-            "CRINGE": "Qualcosa di eccezionalmente strano o imbarazzante",
-            "LOL": "Una risposta comune a qualcosa di divertente",
-            }
-n = 5
+import discord
+from bot_logic import *
 
-for i in range(n):
-    parola = input("Scrivi una parola che non capisci (usa solo lettere maiuscole!): ")
+# la variabile indents contiene i permessi al bot
+intents = discord.Intents.default()
+# abilita il permesso a leggere i contenuti dei messaggi
+intents.message_content = True
+# crea un bot e passa gli indents
+client = discord.Client(intents=intents)
 
-    if parola in meme_dict.keys():
-        print(meme_dict[parola])
+@client.event
+async def on_ready():
+    print(f"Abbiamo fatto l'accesso come {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$ciao'):
+        await message.channel.send("Ciao!")
+    elif message.content.startswith('$arrivederci'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$testa o croce'):
+        await message.channel.send(testa_o_croce())
+    elif message.content.startswith('$emoji casuale'):
+        await message.channel.send(emoji_casuale())
     else:
-        print("la parola che dici tu non la conosciamo")
+        await message.channel.send("La tua password " + gen_pass(10))
+
+client.run("TOKEN_SEGRETO")
+
+#if message.content.startswith('$dammi la password'):
